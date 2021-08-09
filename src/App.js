293ab254d86f3f55c16960b1b10e6dda168f6640
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-08-09 18:34:24 -0400

    files talking to each other, gonna start working on the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import Navbar from './Components/navbar.jsx';
 
 import './assets/App.css';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Navbar />
+      <h1>hello 1 world</h1>
+      <h2>hello 2 world</h2>
+      <h3>hello 3 world</h3>
+      <h4>hello 4 world</h4>
+      <h5>hello 5 world</h5>
+      <p>hello p world</p>
+      <ol>
+        <li>hello 6 world</li>
+        <li>hello 6 world</li>
+        <li>hello 6 world</li>
+        <li>hello 6 world</li>
+      </ol>
     </div>
   );
 }
