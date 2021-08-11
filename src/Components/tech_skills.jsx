7dc379861132ecb92583abc76624bae0309cdb1b
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-08-11 18:03:19 -0400

    made repo public to see if it changes anything

--- src/Components/tech_skills.jsx
+++ src/Components/tech_skills.jsx
@@ -7,7 +7,7 @@ const TechSkills = () => {
   const [techSkills, setTechSkills] = useState([])
 
 useEffect(() => {
-  fetch("https://raw.githubusercontent.com/PascalRacineVenne/pascal_webpage/master/src/data/img_test.json?token=ASMX37YPTJ4J3MGA2WMLB4TBCRDCI")
+  fetch("https://raw.githubusercontent.com/PascalRacineVenne/pascal_webpage/master/src/data/skill.json?token=ASMX377JCKU2EEMCODHOGXLBCRDS2")
     .then(response => response.json())
     .then(data => setTechSkills(data))
 }, [])
