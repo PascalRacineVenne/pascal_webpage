b6e1b0627134d184e02f3646e90e31ad6e6189b5
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-08-11 08:33:22 -0400

    working on the JSON file to fetch the webdev logos to the page

--- src/Components/tech_skills.jsx
+++ src/Components/tech_skills.jsx
@@ -1,87 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Tools from './tools'
 import "./tech_skills.css";
-// import skills from '../skill_data.json';
 
-const skills = [
-  {
-    id: 1,
-    name: "HTML",
+// import skills from '../data/skill_data.json';
 
-  },
-  {
-    id: 2,
-    name: "CSS"
 
-  },
-  {
-    id: 3,
-    name: "JavaScript"
+const TechSkills = () => {
+  const [techSkills, setTechSkills] = useState([])
 
-  },
-  {
-    id: 4,
-    name: "React"
+  console.log(techSkills);
 
-  },
-  {
-    id: 5,
-    name: "HTML",
+useEffect(() => {
+  fetch("../data/skill_data.json ")
+    .then(response => response.json())
+    .then(data => setTechSkills(data))
+}, [])
 
-  },
-  {
-    id: 6,
-    name: "CSS"
 
-  },
-  {
-    id: 7,
-    name: "JavaScript"
-
-  },
-  {
-    id: 8,
-    name: "React"
-
-  },
-  {
-    id: 9,
-    name: "React"
-
-  },
-  {
-    id: 10,
-    name: "React"
-
-  },
-  {
-    id: 11,
-    name: "React"
-
-  },
-  {
-    id: 12,
-    name: "React"
-
-  },
-  {
-    id: 13,
-    name: "React"
-
-  },
-  {
-    id: 14,
-    name: "React"
-
-  }
-]
-
-function TechSkills() {
   return (
     <section className="skills__StyledSkills">
       <h5 className="numbered-heading">Technological Skills</h5>
       <div className="inner_tech_skills">
-      {skills.map((skill) => {
+      {techSkills.map((skill) => {
         return (
           <Tools skill={skill} key={skill.id}/>
         )
@@ -92,3 +32,76 @@ function TechSkills() {
 }
 
 export default TechSkills;
+
+// const skills = [
+//   {
+//     id: 1,
+//     name: "HTML",
+
+//   },
+//   {
+//     id: 2,
+//     name: "CSS"
+
+//   },
+//   {
+//     id: 3,
+//     name: "JavaScript"
+
+//   },
+//   {
+//     id: 4,
+//     name: "React"
+
+//   },
+//   {
+//     id: 5,
+//     name: "HTML",
+
+//   },
+//   {
+//     id: 6,
+//     name: "CSS"
+
+//   },
+//   {
+//     id: 7,
+//     name: "JavaScript"
+
+//   },
+//   {
+//     id: 8,
+//     name: "React"
+
+//   },
+//   {
+//     id: 9,
+//     name: "React"
+
+//   },
+//   {
+//     id: 10,
+//     name: "React"
+
+//   },
+//   {
+//     id: 11,
+//     name: "React"
+
+//   },
+//   {
+//     id: 12,
+//     name: "React"
+
+//   },
+//   {
+//     id: 13,
+//     name: "React"
+
+//   },
+//   {
+//     id: 14,
+//     name: "React"
+
+//   }
+// ]
