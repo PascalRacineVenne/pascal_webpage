0dac42b92bc0b0494cfce8693d212832082f3226
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-08-12 20:17:47 -0400

    cleaned the path for JSON files, all targeted from the files within ... no fetch

--- src/Components/projects_list.jsx
+++ src/Components/projects_list.jsx
@@ -1,21 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Project from './project';
 import "./projects_list.css";
 
+import dataProjects from '../data/projects.json';
+
 const ProjectsList= () => {
-  const [projects, setProjects] = useState([])
-
-  useEffect(() => {
-    fetch("https://raw.githubusercontent.com/PascalRacineVenne/pascal_webpage/master/src/data/projects.json")
-      .then(response => response.json())
-      .then(data => setProjects(data))
-  }, [])
-
   return (
     <section id="projects" className="projects__StyledProjects">
       <h5 className="numbered-heading">Past and current Projects</h5>
       <ul className="inner_projects">
-        {projects.map(project => {
+        {dataProjects.map(project => {
           return(
             <Project project={project} key={project.id} />
           )
--- src/Components/tech_skills.jsx
+++ src/Components/tech_skills.jsx
@@ -1,22 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Tools from './tools'
 import "./tech_skills.css";
 
+import dataSkill from '../data/skill_data.json';
+
 
 const TechSkills = () => {
-  const [techSkills, setTechSkills] = useState([])
-
-  useEffect(() => {
-    fetch("https://raw.githubusercontent.com/PascalRacineVenne/pascal_webpage/master/src/data/skill_data.json")
-      .then(response => response.json())
-      .then(data => setTechSkills(data))
-  }, [])
-
   return (
     <section id="skills" className="skills__StyledSkills">
       <h5 className="numbered-heading">Technological Skills</h5>
       <div className="inner_tech_skills">
-        {techSkills.map((skill) => {
+        {dataSkill.map((skill) => {
           return (
             <Tools skill={skill} key={skill.id} />
           )
