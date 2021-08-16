dcb93d46c56cb214fc56bc56b6871fc7b1986d6a
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-08-15 20:26:41 -0400

    added FontAwesome React magic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "./Components/FontAwesomeIcons";
+
 import AboutMe from './Components/about_me.jsx';
 import Contact from './Components/contact.jsx';
 import Footer from './Components/footer.jsx';
@@ -6,7 +8,6 @@ import Hero from './Components/hero.jsx';
 import Navbar from './Components/navbar.jsx';
 import ProjectsList from './Components/projects_list.jsx';
 import TechSkills from './Components/tech_skills.jsx';
-
 
 import './assets/App.css';
 
--- src/Components/social.jsx
+++ src/Components/social.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import './social.css';
 
 const Social = ( {social} ) => {
   return (
     <span>
-      <a href={social.URL} target="_blank" rel="noopener noreferrer"><p>{social.name}</p></a>
+      <a href={social.URL} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={social.fontAwesomeIcons} /></a>
     </span>
   )
 }
