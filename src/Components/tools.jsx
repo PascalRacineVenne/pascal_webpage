6ae70340faabf8335b90d0b5c15c8c1b89f93e43
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-08-15 21:25:59 -0400

    almost there, missing a few icons from the brand serie

--- src/Components/tools.jsx
+++ src/Components/tools.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './tools.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-// import TestLogo from "../images/web_logo/07-redux_5050.svg"
-// import TestLogo from "../images/web_logo/01-html.svg"
+
+import './tools.css';
 
 const Tools = ({ skill }) => {
   return (
     <div className ="tools__styledTools">
       <div className="tools__circle">
-        <img src={skill.imgURL} alt="logo" />
+        <FontAwesomeIcon icon={skill.fontAwesomeIcons} size="3x" />
       </div>
       <p>{skill.name}</p>
     </div>
