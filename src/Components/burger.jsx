49db2d14104971d76ace0024f0aa3128ab808e02
Maxime Legault-Venne <maximelegaultvenne@datawire.io>
2021-09-14 12:52:03 -0400

    Fix menu display toggle
    There are three main elements in this fix. The first one is the toggle
    action to call `setMenuOpen`. Since it needs to be done on the `button`
    element of the `Burger` component, I think it makes more sense to pass
    the reference to the state value and setter so that the `Burger` can
    update the state of its parent `Menu`. It's then the `Burger`'s
    responsibility to call `setMenuOpen` with the right value when clicked.
    
    Then comes the second part of the fix. In order to leverage animations
    and style overrides in CSS, it's better to use classes rather than
    styles directly. One of the reasons why I find it easier to work with
    classes is that with a combination of classes you can override some
    style definitions, whereas the `style` attribute of a component
    overrides all other values. Once both classes are defined (I called them
    `menuContainer` and `menuContainer__Visible` in this case), we can
    leverage the `menuOpen` state to either assign one class or both of them
    to the `aside` element. To me, it's the easiest way to leverage
    Javascript to toggle styles, leaving all the CSS definitions in distinct
    classes.

--- src/Components/burger.jsx
+++ src/Components/burger.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./burger.css";
 
-const Burger = () => {
+const Burger = ({menuOpen, setMenuOpen}) => {
   return (
-    <button className="burger__StyledBurger">
+    <button className="burger__StyledBurger" onClick={() => setMenuOpen(!menuOpen)}>
       <div className="ham-box">
         <div className= "ham-box-inner"></div>
       </div>
--- src/Components/menu.jsx
+++ src/Components/menu.jsx
@@ -6,37 +6,14 @@ import './menu.css';
 
 const Menu = () => {
 const [menuOpen, setMenuOpen] = useState(false);
-const toggleMenu = () => setMenuOpen[!menuOpen];
-
-  const menuStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'fixed',
-    bottom: '0px',
-    right: '0px',
-    padding: '50px 10px',
-    width: 'min(70vw, 400px)',
-    height: '100vh',
-    outline: '0px',
-    backgroundColor: 'var(--primary_mid_green)',
-    boxShadow: '-10px 0px 30px -15px var(--primary-mid-green)',
-    zIndex: '9',
-    // some prop magic need to happen here
-    // transform: `translateX(${props => (props.menuOpen ? 0 : 100)}vw)`,
-    transform: 'translateX(100vw)',
-    // visibility: `${props => (props.menuOpen ? 'visible' : 'hidden')}`,
-    visibility: 'hidden',
-    transition: '--transition3'
-  }
 
   return(
     <div className="menu__StyledMenu">
       <Burger
-        onClick={toggleMenu}
+        setMenuOpen={setMenuOpen}
         menuOpen={menuOpen}
       />
-      <aside menuOpen={menuOpen} aria-hidden={!menuOpen} tabIndex={menuOpen ? 1 : -1} style={menuStyle}>
+      <aside aria-hidden={!menuOpen} tabIndex={menuOpen ? 1 : -1} className={menuOpen ? 'menuContainer menuContainer__Visible' : 'menuContainer'}>
         <nav className="menuNav__StyledMenu">
           <ol>
             {navLinks &&
