be43919e840cc3400f774f2f4d62c5fd33f4e801
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-02-24 09:33:30 -0500

    change text-alignment for about me and projects list header + change text content for projects list header

--- src/Components/projects_list.jsx
+++ src/Components/projects_list.jsx
@@ -12,7 +12,7 @@ const ProjectsList= () => {
       <p className="projects__StyledPresentation">
         I've recently completed <a href="https://www.lewagon.com/" className="link_wagon" alt="Le Wagon" target="_blank" rel="noreferrer">Le Wagon</a>'s full-stack coding bootcamp
         and looking forward to applying this new knowledge.
-        Since then, I have had the privilege of joining the Wagon team as an assistant teacher and workshop instructor.
+        Since then, I have had the privilege of joining the Wagon team as a teacher, an assistant teacher and workshop instructor.
         These days, I'm seeking to join a team as a full stack web developer
         to deepen and share knowledge of these languages by connecting
         my synthesis and teaching skills to this new career.
