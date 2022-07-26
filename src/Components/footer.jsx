974fbdf944508aa321470793363daad1e9721d25
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-07-26 19:31:43 -0400

    updated date on footer

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import Social from './social';
-import "./footer.css";
+import './footer.css';
 
 import dataSocial from '../data/social.json';
 
 const Footer = () => {
   return (
-    <div className="footer_StyledFooter">
-      <div className="footer__centered">
-        <div className="social__StyledMediaSocial">
+    <div className='footer_StyledFooter'>
+      <div className='footer__centered'>
+        <div className='social__StyledMediaSocial'>
           {dataSocial.map((social) => {
-            return(
-              <Social social={social} key={social.id}/>
-            )
+            return <Social social={social} key={social.id} />;
           })}
         </div>
-        <div className="footer_StyledCopyright">
-         <a href="https://github.com/PascalRacineVenne" alt="github_link" target="_blank" rel="noopener noreferrer">
-          <p>&copy; 2021 Pascal Racine-Venne</p>
-         </a>
+        <div className='footer_StyledCopyright'>
+          <a
+            href='https://github.com/PascalRacineVenne'
+            alt='github_link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <p>&copy; 2022 Pascal Racine-Venne</p>
+          </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
